b9fff94edd4e6584f60c6044fcf73dcb6d69a02d
nam2626 <nam2626@gmail.com>
2021-03-12 11:16:41 +0900

    Default Client Main

--- src/step1/ClientMain.java
+++ src/step1/ClientMain.java
@@ -1,12 +1,43 @@
 package step1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class ClientMain {
 
 	public static void main(String[] args) {
-		//서버 접속
-		//스트림 초기화
-		//서버와 입출력
-		//close
+		Socket client = null;
+		BufferedReader br = null;
+		PrintWriter pw = null;
+		try {
+			//서버 접속
+			client = new Socket("127.0.0.1",1234);
+			//스트림 초기화
+			pw = new PrintWriter(client.getOutputStream());
+			br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			//서버와 입출력
+			pw.println("send message");
+			pw.flush();
+			String msg = br.readLine();
+			System.out.println(msg);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			//close
+			try {
+				if(pw!=null)pw.close();
+				if(br!=null)br.close();
+				if(client!=null)client.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
